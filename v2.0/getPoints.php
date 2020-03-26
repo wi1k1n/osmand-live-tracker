1e0a169db065e8856c0d3bd07df4355999a3c71e
Ilia Mazlov <vfpkjd@gmail.com>
2020-03-26 17:39:01 +0100

    server: getPoints.php: returns tracks+points; track.php: post track name; client: pick last track to show; better error handling

--- v2.0/getPoints.php
+++ v2.0/getPoints.php
@@ -5,42 +5,66 @@
     $starting = isset($_GET['starting']) ? $_GET['starting'] : null;
     $ending = isset($_GET['ending']) ? $_GET['ending'] : null;
 
+    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json');
+
     // Connect to database
     $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
     if ($conn->connect_error) {
-        die("[Error] Failed to connect to DB: " . $conn->connect_error);
-        //die("[Error] Failed to connect to DB");
+        // die('{"error": "Failed to connect to DB: ' . $conn->connect_error . '"}');
+        die('{"error": "Failed to connect to Database"}');
     }
 
-    $sql = "SELECT * FROM osmand_online";
+    // TODO: better behaviour is a separate request for tracks and separate request for only corresponding points
+    // >>> Get table of tracks
+    $tracks = '[';
+    $sql = "SELECT * FROM `osmand_tracks`";
+    $res = $conn->query($sql);
+    if (!$res) die('{"error": "Failed to get data (tracks) from Database"}');
+    if ($res->num_rows === 0) die('{"tracks": [], "points": []}');
+    while ($row = $res->fetch_assoc()) {
+        $tracks = $tracks . '{"uid": ' . $row['uid']
+                         . ', "name": ' . ($row['name'] ? ('"'.$row['name'].'"') : 'null')
+                         . ', "from_lon": ' . ($row['from_lon'] ? ('"'.$row['from_lon'].'"') : 'null')
+                         . ', "from_lat": ' . ($row['from_lat'] ? ('"'.$row['from_lat'].'"') : 'null')
+                         . ', "to_lon": ' . ($row['to_lon'] ? ('"'.$row['to_lon'].'"') : 'null')
+                         . ', "to_lat": ' . ($row['to_lat'] ? ('"'.$row['to_lat'].'"') : 'null')
+                         . '},';
+    }
+    if (is_object($res)) $res->close();
+    $tracks = substr($tracks, 0, strlen($tracks) - 1) . "]";
 
+
+    // >>> Get datapoints
+    $sql = "SELECT * FROM `osmand_online`";
+
+    // Handle $starting, $ending $_GET variables
     if ($starting || $ending) $sql .= " WHERE";
     if ($starting) $sql .= " uid > " . $starting . ' AND';
     if ($ending) $sql .= " uid < " . $ending . ' AND';
     if ($starting || $ending) $sql = substr($sql, 0, strlen($sql) - 4);
-	
+
     $res = $conn->query($sql);
-
-	if ($res->num_rows === 0) {
-	    echo "[]";
-	    exit;
-	}
-
-	$output = "[";
-    while ($row = $res->fetch_assoc()) {
-    	$output = $output . '{"uid": ' . $row['uid']
-    					 . ', "timestamp_server": ' . ($row['timestamp_server'] ? ('"'.$row['timestamp_server'].'"') : 'null')
-                         . ', "timestamp_log": ' . ($row['timestamp_log'] ? ('"'.$row['timestamp_log'].'"') : 'null')
-                         . ', "lat": ' . ($row['lat'])
-                         . ', "lon": ' . ($row['lon'])
-                         . ', "hdop": ' . ($row['hdop'] ? $row['hdop'] : 'null')
-                         . ', "altitude": ' . ($row['altitude'] ? $row['altitude'] : 'null')
-                         . ', "speed": ' . ($row['speed'] ? $row['speed'] : 'null')
-                         . ', "sender": ' . ($row['sender'] ? ('"'.$row['sender'].'"') : 'null')
-                         . '},';
+    if (!$res) die('{"error": "Failed to get data (points) from Database"}');
+    $points = '[]';
+	if ($res->num_rows > 0) {
+        $points = '[';
+        while ($row = $res->fetch_assoc()) {
+        	$points = $points . '{"uid": ' . $row['uid']
+        					 . ', "timestamp_server": ' . ($row['timestamp_server'] ? ('"'.$row['timestamp_server'].'"') : 'null')
+                             . ', "timestamp_log": ' . ($row['timestamp_log'] ? ('"'.$row['timestamp_log'].'"') : 'null')
+                             . ', "lat": ' . ($row['lat'])
+                             . ', "lon": ' . ($row['lon'])
+                             . ', "hdop": ' . ($row['hdop'] ? $row['hdop'] : 'null')
+                             . ', "altitude": ' . ($row['altitude'] ? $row['altitude'] : 'null')
+                             . ', "speed": ' . ($row['speed'] ? $row['speed'] : 'null')
+                             . ', "sender": ' . ($row['sender'] ? ('"'.$row['sender'].'"') : 'null')
+                             . ', "track_uid": ' . ($row['track_uid'] ? ('"'.$row['track_uid'].'"') : 'null')
+                             . '},';
+        }
+        if (is_object($res)) $res->close();
+        $points = substr($points, 0, strlen($points) - 1) . "]";
     }
-    $res->close();
 
-    header("Access-Control-Allow-Origin: *");
-    header('Content-Type: application/json');
-    echo substr($output, 0, strlen($output) - 1) . "]";
+    $output = '{"tracks": '.$tracks.', "points": '.$points.'}';
+    echo $output;
--- v2.0/track.php
+++ v2.0/track.php
@@ -1,40 +1,73 @@
 <?php
     require "configuration.php";
 
+    // Check secret key
     $key = isset($_GET['key']) ? $_GET['key'] : null;
-    if (!isset($key) || $key == 'CHANGEME') {
-        die('[Error] Key not set');
-    }
+    if (!isset($key) || $key == 'CHANGEME') die('[Error] Key not set');
+    if ($key != $secretKey) die('[Error] Invalid key');
 
-    if ($key != $secretKey) {
-        print '[Error] Invalid key';
-        return;
-    }
-
+    // Parse $_GET values
     $data = array(
         'lat' => isset($_GET['lat']) ? "'".floatval($_GET['lat'])."'" : 'NULL',
         'lon' => isset($_GET['lon']) ? "'".floatval($_GET['lon'])."'" : 'NULL',
-        'timestamp' => isset($_GET['timestamp']) ? "'".date('Y-m-d H:i:s', time()-intval($_GET['timestamp'])/1000.0)."'" : 'NULL',
+        'timestamp' => isset($_GET['timestamp']) ? "'".date('Y-m-d H:i:s', time()-intval($_GET['timestamp'])/1000.0)."'" : "current_timestamp()",
         'hdop' => isset($_GET['hdop']) ? "'".floatval($_GET['hdop'])."'" : 'NULL',
         'altitude' => isset($_GET['altitude']) ? "'".floatval($_GET['altitude'])."'" : 'NULL',
         'speed' => isset($_GET['speed']) ? "'".floatval($_GET['speed'])."'" : 'NULL',
         'sender' => isset($_GET['sender']) ? "'".$_GET['sender']."'" : 'NULL',
-        'track_uid' => isset($_GET['track_uid']) ? "'".$_GET['track_uid']."'" : 0
+        'track' => isset($_GET['track']) ? "'".$_GET['track']."'" : null
     );
 
-    if (!isset($data['lat']) || !isset($data['lon'])) {
-        print '[Error] \'lon\' and \'lat\' arguments are mandatory!';
-        return;
-    }
+    // Longitude and Latitude values are mandatory
+    if (!isset($data['lat']) || !isset($data['lon'])) die('[Error] \'lon\' and \'lat\' arguments are mandatory!');
 
-    // var_dump($data);
-
+    // Connect to database
     $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
     if ($conn->connect_error) {
         die("[Error] Failed to connect to DB");//: " . $conn->connect_error);
     }
 
-    $query = "INSERT INTO `osmand_online`(`timestamp_server`, `timestamp_log`, `lat`, `lon`, `hdop`, `altitude`, `speed`, `sender`, `track_uid`) VALUES(current_timestamp(), ".$data['timestamp'].", ".$data['lat'].", ".$data['lon'].", ".$data['hdop'].", ".$data['altitude'].", ".$data['speed'].", ".$data['sender'].", ".$data['track_uid'].")";
+    // Makes query to DB and returns null if track was not found, or DB-row otherwise
+    function getTrackEntry($conn, $trackName) {
+        $sql = "SELECT * FROM `osmand_tracks` WHERE name=".$trackName;
+        $res = $conn->query($sql);
+        if ($res && is_object($res)) {
+            if ($res->num_rows === 0) {
+                $res->close();
+                return null;
+            }
+            else {
+                $row = $res->fetch_assoc();
+                $res->close();
+                return $row;
+            }
+        }
+        if (is_object($res)) $res->close();
+        return null;
+    }
+
+    $track_uid = 0; // 0 corresponds to Unknown entry in DB
+    // Get track_uid from DB if track name is specified in $_GET
+    if (isset($data['track'])) {
+        $track = getTrackEntry($conn, $data['track']);
+        if (isset($track['uid']))
+            $track_uid = $track['uid'];
+        else { // Track was not found in DB
+            // Then create new entry ourselves
+            $sql = "INSERT INTO `osmand_tracks`(`name`) VALUES(".$data['track'].")";
+            $res = $conn->query($sql);
+            if ($res) {
+                // Insertion of new track entry successfull
+                $track_new = getTrackEntry($conn, $data['track']);
+                if (isset($track_new) && isset($track_new['uid']))
+                    $track_uid = $track_new['uid'];
+            }
+            // If failed, then leave everything as it is. UID of 'Unknown' is already stored in $track_uid
+            if (is_object($res)) $res->close();
+        }
+    }
+
+    $query = "INSERT INTO `osmand_online`(`timestamp_server`, `timestamp_log`, `lat`, `lon`, `hdop`, `altitude`, `speed`, `sender`, `track_uid`) VALUES(current_timestamp(), ".$data['timestamp'].", ".$data['lat'].", ".$data['lon'].", ".$data['hdop'].", ".$data['altitude'].", ".$data['speed'].", ".$data['sender'].", ".$track_uid.")";
     $res = $conn->query($query);
     if (!$res) {
         // echo "[Error] Failed to insert data to DB.";
@@ -43,5 +76,5 @@
     if (is_object($res)) {
         $res->close();
     }
-
     $conn -> close();
+    echo '';
