144f24213aaba2a7338e49c29ad89a058425e5d2
Ilia Mazlov <vfpkjd@gmail.com>
2020-03-29 15:24:03 +0200

    no sensitive data anymore; replaced with placeholders

---
+++ v2.0/configuration.php
@@ -0,0 +1,11 @@
+<?php
+    global $secretKey, $filePath, $name, $zoom, $accuracy, $height, $width, $dbName, $dbUser, $dbHost, $dbPassword;
+
+    // This key must be entered in OsmAnd's tracking URL as &key=
+    $secretKey = 'CHANGEME';
+
+    // Database credentials
+    $dbName = 'CHANGEME';
+    $dbUser = 'CHANGEME';
+    $dbHost = 'localhost';
+    $dbPassword = 'CHANGEME';
