f6a198f6c689cc7c52e131b14e619c61e05b199e
Ilia Mazlov <vfpkjd@gmail.com>
2020-03-30 20:01:04 +0200

    plotly: cursor synchronization

--- v2.0/constants.js
+++ v2.0/constants.js
@@ -1,5 +1,5 @@
 const URL_GETPOINTS = 'http://batukah.000webhostapp.com/osholm/v2.0/getPoints.php';
 const CURSOR_TOLERANCE = 4000;
-const DEBUG = false;
+const DEBUG = true;
 const HDOP_TITLES = ['Excellent accuracy', 'Average accuracy', 'Poor accuracy'];
 const HDOP_COLORS = ['green', 'rgb(180,162,33)', 'red'];
